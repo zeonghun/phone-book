3abc2736eab3620edc78c75290867048a2861a1b
Jeonghun Kim <zeonghun413@gmail.com>
2023-02-10 15:41:46 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// 유저는 연락처를 생성할 수 있다
+// 연락처에는 이름과 전화번호 정보가 필요하다
+// 유저는 연락처 리스트를 볼 수 있다
+// 유저는 이름으로 연락처를 검색할 수 있다
+
+function App() {
+  return (
+    <div>
+      <h1>Phone Book</h1>
+    </div>
+  );
+}
+
+export default App;
